cc6c1b14348b315913243f6c15361bcffe05ea13
nicosis <nicosis@gmail.com>
2023-03-19 16:29:26 +0000

    solucion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 //En este array de strings, tenemos todos los colores
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const colors = [ 'red', 'green', 'blue', 'yellow', 'pink', 'purple' ];
 
+randomColor = () => {
+    let randomIndex = Math.floor(Math.random() * (colors.length));
+    return colors[ randomIndex ];
+};
 
+let button = document.querySelector('button');
+let bodyColor = document.querySelector('body');
+
+button.addEventListener('click', (event) => {
+    bodyColor.style.backgroundColor = randomColor();
+});
